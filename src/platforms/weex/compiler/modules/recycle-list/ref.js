8beb1abf9eed9eaf34a00753530a5bec0eeccb3f
Zhenfei You <hello@imyzf.com>
2018-07-23 17:12:57 +0800

    fix(weex): fix ref undefined in child component of recycle-list

--- src/platforms/weex/compiler/modules/recycle-list/ref.js
+++ src/platforms/weex/compiler/modules/recycle-list/ref.js
@@ -5,5 +5,6 @@ import { addAttr } from 'compiler/helpers'
 export function postTransformRef (el: ASTElement, options: WeexCompilerOptions) {
   if (el.ref) {
     addAttr(el, 'ref', el.ref)
+    delete el.ref
   }
 }
